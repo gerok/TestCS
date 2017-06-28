a397f5372322e018480416a597be6df379c8759a
gerok <gerok@linuxtest2017.strose.edu>
2017-06-28 14:42:20 -0400

    Added a print ln

---
+++ TestCSNew/src/testJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author gerok
+ *
+ */
+public class testJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//yay!!!
+		System.out.println("yay");
+	}
+
+}
